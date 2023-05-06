eafdf65892eb245aa821271fa113ab4379579b1c
harsh-android <harshche123@gmail.com>
2023-05-06 18:10:34 +0530

    Initial commit

---
+++ app/src/main/java/com/hinfo/googlemap/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.hinfo.googlemap
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    val locationPermissionCode = 2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getLocation()
+
+    }
+
+    private fun getLocation() {
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED)
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.    Manifest.permission.ACCESS_FINE_LOCATION),
+                locationPermissionCode
+            )
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, object : LocationListener{
+            override fun onLocationChanged(location: Location) {
+
+            }
+
+        })
+    }
+
+}
